3b46276fe9c88d3eadf4b2855b3de5cff8b548cf
Ngo Tai Hung <ngotaihung@gmail.com>
2023-05-08 19:24:24 +0700

    Update redis for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ "https://video-sharing-nthung.herokuapp.com", "http://video-sharing-nthung.herokuapp.com" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -92,4 +92,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = 'wss://video-sharing-nthung.herokuapp.com/cable'
 end
